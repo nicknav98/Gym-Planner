f9e10bec6795a63179d3990768b33f5f5f4fb20f
Jerry Stigell <jerrystigell@Jerry-MBP.lan>
2020-11-21 12:40:34 +0200

    updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_restful import Api
+
+
+from resources.workout import WorkoutListResource, WorkoutResource, WorkoutPublishResource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "Hello World"
+api.add_resource(WorkoutListResource, '/workouts')
+api.add_resource(WorkoutResource, '/workouts/<int:workout_id>')
+api.add_resource(WorkoutPublishResource, '/workouts/<int:workout_id>/publish')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000, debug=True)
