ffadc40e9e9da283b19e4ef59bff9fb9fe52775d
Nicholas <nicholas.navarro@edu.turkuamk.fi>
2020-11-21 12:45:37 +0200

    updated workout.py, error deletion

--- resources/workout.py
+++ resources/workout.py
@@ -1,29 +1,29 @@
 from flask import request
 from flask_restful import Resource
 from http import HTTPStatus
-from models.workouts import workouts, workouts_list
+from models.workouts import Workout, workout_list
 
 class WorkoutListResource(Resource):
     def get(self, workout=None):
         data = []
-        for workouts in workouts_list:
-        if workout.is_publish is True:
-            data.append(workout.data)
+        for workouts in workout_list:
+            if workout.is_publish is True:
+                data.append(workout.data)
         return {'data': data}, HTTPStatus.OK
 
     def post(self):
         data = request.get_json()
-        workouts = Workouts(name=data['name'],
-                length=data['length'],
-                directions=data['directions'],
-                type=data['type'],
-                is_publish=data['is_publish'])
-        workouts_list.append(workouts)
+        workouts = Workout(name=data['name'],
+                           length=data['length'],
+                           directions=data['directions'],
+                           type=data['type'],
+                           is_publish=data['is_publish'])
+        workout_list.append(workouts)
         return workouts.data, HTTPStatus.CREATED
 
 class WorkoutResource(Resource):
     def get(self, workouts_id):
-        workouts = next((workouts for workouts in workouts_list if workouts.id ==
+        workouts = next((workouts for workouts in workout_list if workouts.id ==
         workouts_id and workouts.is_publish == True), None)
         if workouts is None:
             return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
@@ -31,7 +31,7 @@ class WorkoutResource(Resource):
 
     def put(self, workouts_id):
         data = request.get_json()
-        workouts = next((workouts for workouts in workouts_list if workouts.id == workouts_id), None)
+        workouts = next((workouts for workouts in workout_list if workouts.id == workouts_id), None)
         if workouts is None:
             return {'message': 'workouts not found'}, HTTPStatus.NOT_FOUND
         workouts.name = data['name']
@@ -43,7 +43,7 @@ class WorkoutResource(Resource):
 
 class WorkoutPublishResource(Resource):
     def put(self, recipe_id):
-        workouts = next((workouts for workouts in workouts_list if workouts.id == workouts_id), None)
+        workouts = next((workouts for workouts in workout_list if Workout.id == Workout.id), None)
         if workouts is None:
             return {'message': 'workouts not found'}, HTTPStatus.NOT_FOUND
         workouts.is_publish = True
