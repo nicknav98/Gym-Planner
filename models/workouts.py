b2f40d644e2806bb62c820f3981da30269359986
Jerry Stigell <jerrystigell@Jerry-MBP.lan>
2020-11-21 11:59:58 +0200

    new models folder

---
+++ models/workouts.py
@@ -0,0 +1,30 @@
+workout_list = []
+
+def get_last_id():
+    if workout_list:
+        last_workout = workout_list[-1]
+    else:
+        return 1
+    return last_workout.id +1
+
+class Workout:
+    def __init__(self, name, length, directions, type):
+        self.id = get_last_id()
+        self.name = name
+        self.length = length
+        self.directions = directions
+        self.type = type
+        self.is_publish = False
+
+@property
+def data(self):
+    return {
+        'id' : self.id,
+        'name' : self.name,
+        'length' : self.length,
+        'directions' : self.directions,
+        'type' : self.type
+    }
+
+
+
