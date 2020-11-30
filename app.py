3e99ded74cb8284e3b709126aee013081cbd0390
Michalis <michalis.iona@edu.turkuamk.fi>
2020-11-30 15:01:36 +0200

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_restful import Api
 
 from Config import Config
 from extensions import db
-from models.user import User
+from resources.user import UserListResource
 from resources.workout import WorkoutListResource, WorkoutResource, WorkoutPublishResource
 
 def create_app():
@@ -23,10 +23,11 @@ def register_extensions(app):
 def register_resources(app):
     api = Api(app)
 
+    api.add_resource(UserListResource, '/users')
     api.add_resource(WorkoutListResource, '/workouts')
     api.add_resource(WorkoutResource, '/workouts/<int:workout_id>')
     api.add_resource(WorkoutPublishResource, '/workouts/<int:workout_id>/publish')
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(port=5432, debug=True)
+    app.run(port=6000, debug=True)
